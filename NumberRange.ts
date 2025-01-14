e8c43434a29726295144fa1a84f901520d613050
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2025-01-14 07:24:59 +0100

    Update NumberRange.ts

--- NumberRange.ts
+++ NumberRange.ts
@@ -7,3 +7,21 @@ const NumberRange = (start: number, end: number ) => {
 } //CONCAT
 
 console.log( NumberRange(1, 10) ) //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// UNWINDING in Recursion
+
+const NumberRange = (start: number, end: number ) => {
+	if(start === end){
+  	return [end]
+  }
+	//For Understanding
+  // const numbers = []
+  // numbers.push(end)
+  // return NumberRange(start, end - 1).concat(numbers)
+	
+  const numbers = NumberRange(start, end - 1)
+  numbers.push(end)
+  return numbers
+}
+
+console.log( NumberRange(1, 10) )
