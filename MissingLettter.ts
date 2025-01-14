3698aefee3ee08ae52da4e66c5a6483ce7e3f89c
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2025-01-14 02:25:31 +0100

    Update MissingLettter.ts

--- MissingLettter.ts
+++ MissingLettter.ts
@@ -15,3 +15,40 @@ function findMissingLetter(str: string): string | null {
 
 //SOLUTION 2
 
+function findMissingLetter(str: string): string | null {
+  let start = str.charCodeAt(0);
+ 	
+  const missingCharCode = str.split('').map(char => char.charCodeAt(0) ).find((current) => {
+    if(current - start > 1){
+    	return true // returns 101 when str = 'abce' [97,98,99,101] because it passes the check
+      //since 101 - 99 = 2 which is > 1
+    }
+    start = current
+    return false;
+  })
+  
+  console.log(missingCharCode)
+  
+  return missingCharCode ? String.fromCharCode(missingCharCode - 1) : ''
+}
+
+console.log(findMissingLetter('abce'))
+
+// SOLUTION 3
+
+function findMissingLetter(str: string): string | null {
+  let start = str.charCodeAt(0);
+ 	
+  const missingCharCode = str.split('').filter((char, idx) => {
+  	if (idx === 0) return false;// no character behind the first char so thats not what we looking for/
+    const prevChar = str.split('')[ idx - 1 ].charCodeAt(0);
+    const currentChar = char.charCodeAt(0)
+    
+    return currentChar - prevChar > 1
+  })[0]
+  
+  return missingCharCode ? String.fromCharCode(missingCharCode.charCodeAt(0) - 1) : ''
+}
+
+console.log(findMissingLetter('abce'))
+
