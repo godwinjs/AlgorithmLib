37c155d1a986a22ead45eed48f2e19d053e9d4a7
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2025-01-15 02:03:52 +0100

    Update Flatten.ts
    task: for FlattenRefactor Function write a cleaner logic.
    
    gains: reduced the number of times Flatten was called from 15 down to 9 times.
    
    Flatten2 is being called 6 times

--- Flatten.ts
+++ Flatten.ts
@@ -18,4 +18,48 @@ const Flatten = (arr: any[] ) => {
   return Flatten(arr);
 }
 
-console.log( Flaten([1, [2, 3], [4, [5, 6], 7], 8, [9], 10]) ) // returns [1,2,3,4,5,6,7,8,9,10]
+const FlattenRefactor = (arr: any[] ) => {
+  let firstItem = arr.shift() //get the first item in the array
+  let result: number[] = []
+  i++
+  
+
+  if(arr.length <= 0){
+    return firstItem
+  }
+  
+  if(typeof(firstItem) === 'number'){ // if its not not a sub array run block
+    result.push(firstItem)
+  }else{
+  	// else if its an array, distructure it and add it to the begining of the top array
+    for (const item of firstItem ){
+        if(Array.isArray(item)){
+            arr.unshift(...item)
+            return Flatten(arr)
+        }else{
+            result.push(item)
+        }
+    }
+  }
+  
+  return result.concat(Flatten(arr))
+}
+
+console.log( Flatten([1, [2, 3], [4, [5, 6], 7], 8, [9], 10]) ) // returns [1,2,3,4,5,6,7,8,9,10]
+
+//SOLUTION 2
+const Flatten = (arr: any[] ) => {
+	let result = [];
+  
+  for (const item of arr) { //the constrains is handled by the loop
+  	if(Array.isArray(item)){
+    	result = result.concat(Flatten(item) )
+    }else {
+    	result.push(item)
+    }
+  }
+  
+  return result
+}
+
+console.log( Flatten( [1, [2, 3], [4, [5, 6], 7], 8, [9], 10] ) )  // returns [1,2,3,4,5,6,7,8,9,10]
