44ea527339cf22805467d4940ceb59e5848310a2
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2025-01-16 02:21:24 +0100

    Update PowerExponent.ts

--- PowerExponent.ts
+++ PowerExponent.ts
@@ -8,3 +8,25 @@ function Power (base: num, exp: number): number {
 }
 
 console.log( Power(2, 3) ) // 8
+
+// SULUTION 2 O(log n) LOGRITHMIC TIME COMPLEXITY: finding the value of a number raised to a number
+function PowerPower2 (base: num, exp: number): number {
+    // for X^0 return 1
+    if(exp === 0) {
+        return 1
+    }
+
+    /*
+        if the exponent is even you can recursively call the function once to 
+        find a result for (half the exponent of the base) base^exp/2. 
+        and when you multiply the result by itself you get the real Power.
+    */
+	let halfPower;
+    if(exp % 2 === 0) {
+        halfPower = Power(base, exp/2)
+        return halfPower * halfPower
+    }else {
+        halfPower = Power(base, (exp-1)/2) // minus base ^ 1 to make it even
+        return halfPower * halfPower * base // multiply the result with base ^ 1 you subtracted before.
+    }
+}
