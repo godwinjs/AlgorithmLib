7af35ab652ad1dfb671a77a2fd05149d2041d47c
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2025-01-16 01:29:00 +0100

    Update ArithmeticProgression.ts

--- Maths/Progressions/ArithmeticProgression.ts
+++ Maths/Progressions/ArithmeticProgression.ts
@@ -1,6 +1,6 @@
 console.log('Arithmetic Progression for non-constant common difference')
 
-function CalculateNonConstantTerm(n: number, differences: number[] ): number {
+function NonConstantAPTerm(n: number, differences: number[] ): number {
     const a = differences.shift(); // First term
 
     if (n === 1) {
@@ -12,5 +12,5 @@ function CalculateNonConstantTerm(n: number, differences: number[] ): number {
   const differences = [2, 5, 3, 1, 11, 7, 6, 4]; // Differences
   const N_th = 8; // Term to calculate
   
-  const result = calculateTerm(N_th, differences);
+  const result = NonConstantAPTerm(N_th, differences);
   console.log(`The ${N_th}th term is ${result}`);
